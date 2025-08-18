19151cf5defa0a4bac4bd55b2626534f9fa800f6
EleniTsour <elenitsouroupi@gmail.com>
2025-08-18 20:43:45 +0300

    new pdfs

--- app/(pages)/(website)/page.jsx
+++ app/(pages)/(website)/page.jsx
@@ -1,12 +1,23 @@
-import FlipbookViewer from '@/app/_components/ui/flipbook-viewer/flipbook-viewer'
-import React from 'react'
+"use client";
+import FlipbookViewer from "@/app/_components/ui/flipbook-viewer/flipbook-viewer";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const Page = () => {
+  const searchParams = useSearchParams();
+  const book = searchParams.get("book") || "1";
+
+  const pdfMap = {
+    1: "/demo.pdf",
+    2: "/demo2.pdf",
+    3: "/demo3.pdf",
+  };
+
   return (
     <div className="h-screen">
-      <FlipbookViewer pdfUrl='/demo.pdf' />
+      <FlipbookViewer pdfUrl={pdfMap[book]} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
