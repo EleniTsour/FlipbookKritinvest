754862498642601c25a09be67845236ac58a9ecd
EleniTsour <elenitsouroupi@gmail.com>
2025-10-28 03:11:51 +0200

    multiple pdfs4

--- app/(pages)/(website)/page.jsx
+++ app/(pages)/(website)/page.jsx
@@ -1,30 +1,12 @@
-"use client";
+import FlipbookViewer from '@/app/_components/ui/flipbook-viewer/flipbook-viewer'
+import React from 'react'
 
-import { Suspense } from "react";
-import FlipbookViewer from "@/app/_components/ui/flipbook-viewer/flipbook-viewer";
-import { useSearchParams } from "next/navigation";
-
-export const dynamic = "force-dynamic";
-
-function FlipbookClient() {
-  const searchParams = useSearchParams();
-  const book = searchParams.get("book") || "1";
-
-  const pdfMap = {
-    1: "/demo.pdf",
-    2: "/demo2.pdf",
-    3: "/demo3.pdf",
-  };
-
-  return <FlipbookViewer pdfUrl={pdfMap[book]} />;
-}
-
-export default function Page() {
+const Page = () => {
   return (
-    <div className="h-screen">
-      <Suspense fallback={<div>Loading book...</div>}>
-        <FlipbookClient />
-      </Suspense>
+    <div className="block">
+      <FlipbookViewer pdfUrl='/demo.pdf' />
     </div>
-  );
+  )
 }
+
+export default Page
