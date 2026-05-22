2e5f7913a3b8a233a69a1575a5383d1c8290148f
EleniTsour <elenitsouroupi@gmail.com>
2026-05-22 17:00:13 +0300

    fix

--- app/(pages)/(website)/page.jsx
+++ app/(pages)/(website)/page.jsx
@@ -1,31 +1,7 @@
-"use client";
-import FlipbookViewer from "@/app/_components/ui/flipbook-viewer/flipbook-viewer";
-import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
-
-const HomepageViewer = () => {
-  const searchParams = useSearchParams();
-  const book = searchParams.get("book") || "1";
-
-  const pdfMap = {
-    1: "/demo.pdf",
-    2: "/demo2.pdf",
-    3: "/demo3.pdf",
-  };
-
-  return (
-    <div className="h-screen">
-      <FlipbookViewer pdfUrl={pdfMap[book]} />
-    </div>
-  );
-};
+import { redirect } from "next/navigation";
 
 const Page = () => {
-  return (
-    <Suspense fallback={<div className="h-screen" />}>
-      <HomepageViewer />
-    </Suspense>
-  );
+  redirect("/demo");
 };
 
 export default Page;
